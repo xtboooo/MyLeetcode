06c13b5b3cb833d7deb4deff83e6f60d0d36cbc9
tb.xu <xtbo9719@gmail.com>
2022-07-26 22:21:16 +0800

    合并k个有序链表

---
+++ todo/23.合并k个升序链表.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=23 lang=python3
+#
+# [23] 合并K个升序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(
+        self, lists: List[Optional[ListNode]]
+    ) -> Optional[ListNode]:
+        pass
+
+
+# @lc code=end

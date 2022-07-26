a71c74d106d32783813058974ef3fa789d7a32cb
tb.xu <xtbo9719@gmail.com>
2022-07-26 21:07:48 +0800

    判断有效括号

---
+++ 20220726/20.有效的括号.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=20 lang=python3
+#
+# [20] 有效的括号
+#
+
+# @lc code=start
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+
+        pairs = {
+            ")": "(",
+            "]": "[",
+            "}": "{",
+        }
+        stack = []
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+        return not stack
+
+
+# @lc code=end

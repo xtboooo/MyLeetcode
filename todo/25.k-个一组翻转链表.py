08e4e8acd59a4dfde4f1ccb2fa73ee673d27dd8f
tb.xu <xtbo9719@gmail.com>
2022-07-26 22:37:53 +0800

    K 个一组翻转链表

---
+++ todo/25.k-个一组翻转链表.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=25 lang=python3
+#
+# [25] K 个一组翻转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseKGroup(
+        self, head: Optional[ListNode], k: int
+    ) -> Optional[ListNode]:
+        pass
+
+
+# @lc code=end

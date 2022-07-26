eecf0c0d1820d961e223fa6a1534f0d70cab9dec
tb.xu <xtbo9719@gmail.com>
2022-07-26 23:04:36 +0800

    移除元素

---
+++ 20220726/27.移除元素.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        right = len(nums)
+        while left < right:
+            if nums[left] == val:
+                nums[left] = nums[right - 1]
+                right -= 1
+            else:
+                left += 1
+        return left
+
+
+# @lc code=end

d669f8ad95c26b37edeafad8316d94865d22831a
tb.xu <xtbo9719@gmail.com>
2022-07-26 23:28:26 +0800

    haystack and needle

---
+++ todo/28.实现-str-str.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=28 lang=python3
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        pass
+
+
+# @lc code=end

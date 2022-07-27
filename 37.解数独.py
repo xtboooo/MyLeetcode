a53e469b908651aa75516ae1676143d14bcc9d57
tb.xu <xtbo9719@gmail.com>
2022-07-27 18:04:50 +0800

    解数独

---
+++ 37.解数独.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=37 lang=python3
+#
+# [37] 解数独
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass
+
+
+# @lc code=end

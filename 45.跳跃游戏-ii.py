110b33e82c425b043549e80676c4901ac2487069
tb.xu <xtbo9719@gmail.com>
2022-07-27 22:18:19 +0800

    跳跃游戏 II

---
+++ 45.跳跃游戏-ii.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=45 lang=python3
+#
+# [45] 跳跃游戏 II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_pos, end, step = 0, 0, 0
+        for i in range(n - 1):
+            if max_pos >= i:
+                max_pos = max(max_pos, i + nums[i])
+            if i == end:
+                end = max_pos
+                step += 1
+        return step
+
+
+# @lc code=end

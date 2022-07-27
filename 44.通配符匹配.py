407ae76df73677ceae95b24a511136d52d135d6c
tb.xu <xtbo9719@gmail.com>
2022-07-27 22:03:02 +0800

    通配符匹配

---
+++ 44.通配符匹配.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=44 lang=python3
+#
+# [44] 通配符匹配
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
+
+
+# @lc code=end

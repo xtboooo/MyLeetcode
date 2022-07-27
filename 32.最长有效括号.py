5c4ea167ac1cf32fbc6004bde14fe889ee248357
tb.xu <xtbo9719@gmail.com>
2022-07-27 16:11:13 +0800

    最长有效括号

---
+++ 32.最长有效括号.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=32 lang=python3
+#
+# [32] 最长有效括号
+#
+
+# @lc code=start
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        pass
+
+
+# @lc code=end

1f77697a7edee3f27bff726b69c215c46d549f0e
tb.xu <xtbo9719@gmail.com>
2022-07-28 07:29:43 +0800

    N 皇后

---
+++ 51.n-皇后.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=51 lang=python3
+#
+# [51] N 皇后
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        pass
+
+
+# @lc code=end

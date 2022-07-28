71e025dddb3bdf9db2c21dd7e0ee16a09689f206
tb.xu <xtbo9719@gmail.com>
2022-07-28 13:46:13 +0800

    文本左右对齐

---
+++ 68.文本左右对齐.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=68 lang=python3
+#
+# [68] 文本左右对齐
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        ...
+
+
+# @lc code=end

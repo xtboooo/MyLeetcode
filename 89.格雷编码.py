48497de9e943d1d9a7891fd0736103ef8fb6621b
tb.xu <xtbo9719@gmail.com>
2022-07-29 17:11:52 +0800

    格雷编码

---
+++ 89.格雷编码.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=89 lang=python3
+#
+# [89] 格雷编码
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        ...
+
+
+# @lc code=end

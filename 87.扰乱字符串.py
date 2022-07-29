9f98e9f04d8a53c514030bd0d710d068220ee28b
tb.xu <xtbo9719@gmail.com>
2022-07-29 16:36:44 +0800

    扰乱字符串

---
+++ 87.扰乱字符串.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=87 lang=python3
+#
+# [87] 扰乱字符串
+#
+
+# @lc code=start
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        ...
+
+
+# @lc code=end

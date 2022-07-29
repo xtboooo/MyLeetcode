44f7ab5d9054d6f1ab72542d7b691356fb2ad4f7
tb.xu <xtbo9719@gmail.com>
2022-07-29 21:59:20 +0800

    复原 IP 地址

---
+++ 93.复原-ip-地址.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=93 lang=python3
+#
+# [93] 复原 IP 地址
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ...
+
+
+# @lc code=end

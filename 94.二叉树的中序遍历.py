ddb60a182cee53dac61a88a2f405663ed4c538f5
tb.xu <xtbo9719@gmail.com>
2022-07-30 12:48:28 +0800

    二叉树的中序遍历

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+
+import re
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# 递归
+class Solution1:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(curr):
+            if not curr:
+                return
+            dfs(curr.left)
+            res.append(curr.val)
+            dfs(curr.right)
+
+        res = []
+        dfs(root)
+        return res
+
+
+# 迭代
+class Solution2:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+
+
+# 标记法
+class Solution3:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = [(0, root)]
+        while stack:
+            flag, curr = stack.pop()
+            if not curr:
+                continue
+            if flag == 0:
+                stack.append((0, curr.right))
+                stack.append((1, curr))
+                stack.append((0, curr.left))
+            else:
+                res.append(curr.val)
+        return res
+
+
+# @lc code=end

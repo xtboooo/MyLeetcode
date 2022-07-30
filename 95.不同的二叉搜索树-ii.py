4e81c556fafe741ec44b7e00e6ecb5de1f947130
tb.xu <xtbo9719@gmail.com>
2022-07-30 13:22:35 +0800

    不同的二叉搜索树2

---
+++ 95.不同的二叉搜索树-ii.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=95 lang=python3
+#
+# [95] 不同的二叉搜索树 II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        ...
+
+
+# @lc code=end

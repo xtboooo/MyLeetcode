380c7cc858674bc28f736deab374912fd9d1a405
tb.xu <xtbo9719@gmail.com>
2022-08-01 22:04:39 +0800

    1-15 添加题目要求

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -6,6 +6,22 @@
 
 # @lc code=start
 
+"""
+Medium
+
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+"""
+
 
 class Solution:
     def expend_center(self, s, left, right):

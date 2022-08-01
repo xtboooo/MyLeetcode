f894615a00ff0a9b1090982006999e53e518d70b
tb.xu <xtbo9719@gmail.com>
2022-08-01 22:30:37 +0800

    16-30 添加题目解释

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -6,7 +6,25 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-from codecs import latin_1_decode
+"""
+Easy
+
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+"""
+
 from typing import Optional
 
 

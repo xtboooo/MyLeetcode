d547fc88b4953fc2242ba573d9fcdab49885a018
tb.xu <xtbo9719@gmail.com>
2022-08-03 12:07:25 +0800

    相交链表

---
+++ easy/160.相交链表.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=160 lang=python3
+#
+# [160] 相交链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# 双指针
+class Solution:
+    def getIntersectionNode(
+        self, headA: ListNode, headB: ListNode
+    ) -> ListNode:
+        a, b = headA, headB
+        while a != b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
+
+
+# @lc code=end

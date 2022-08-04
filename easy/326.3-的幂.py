dd1cff80a4234e5411ce0836d3904ddac29a9050
tb.xu <xtbo9719@gmail.com>
2022-08-04 10:53:59 +0800

    3 的幂

---
+++ easy/326.3-的幂.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode.cn id=326 lang=python3
+#
+# [326] 3 的幂
+#
+
+# @lc code=start
+"""
+给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+
+整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+
+示例 1：
+
+输入：n = 27
+输出：true
+示例 2：
+
+输入：n = 0
+输出：false
+示例 3：
+
+输入：n = 9
+输出：true
+示例 4：
+
+输入：n = 45
+输出：false
+"""
+
+
+# 除法
+class Solution1:
+    def isPowerOfThree(self, n: int) -> bool:
+        while n and n % 3 == 0:
+            n //= 3
+        return n == 1
+
+
+# 最大公约数
+class Solution2:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 1162261467 % n == 0
+
+
+# @lc code=end

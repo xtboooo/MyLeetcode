f6d6cfcc7b986f6cf0416efc3049baaad0897bab
uian23 <xtbo9719@gmail.com>
2022-08-12 23:59:20 +0800

    od真题77

--- 真题/华为od真题.py
+++ 真题/华为od真题.py
@@ -409,3 +409,63 @@ for i, (h, m, s) in enumerate(time_list):
 index_list = map(lambda y: y[0], sorted(d.items(), key=lambda x: x[1]))
 for index in index_list:
     print(time_dict[index])
+
+
+"""
+  双十一众多商品进行打折销售
+  小明想购买自己心仪的一些物品
+  但由于购买资金限制
+  所以他决定从众多心仪商品中购买三件
+  而且想尽可能得花完资金
+  现在请你设计一个程序 计算小明尽可能花费的最大资金数
+
+  输入描述：
+    输入第一行为一维整型数组m
+    数组长度小于100
+    数组元素记录单个商品的价格
+    单个商品加个小于1000
+
+    输入第二行为购买资金的额度r
+    r<100000
+
+  输出描述：
+     输出为满足上述条件的最大花费额度
+
+   注意：如果不存在满足上述条件的商品请返回-1
+
+  示例：
+     输入
+      23,26,36,27
+      78
+     输出
+      76
+     说明：
+      金额23、26、27得到76而且最接近且小于输入金额78
+
+   示例：
+       输入
+       23,30,40
+       26
+       输出
+        -1
+       说明
+       因为输入的商品无法满足3件之和小于26
+       故返回-1
+
+   输入格式正确无需考虑输入错误情况
+"""
+
+nums = list(map(int, input().split(",")))
+target = int(input())
+
+n = len(nums)
+if n < 3:
+    print(nums)
+ans = -1
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            tmp = nums[i] + nums[j] + nums[k]
+            if tmp <= target:
+                ans = max(ans, tmp)
+print(ans)

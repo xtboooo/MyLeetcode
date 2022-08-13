cc4327a13d85cdf95a8f1a3edaffaa04b5f127f5
uian23 <xtbo9719@gmail.com>
2022-08-13 11:25:26 +0800

    真题68

--- 真题/华为od真题.py
+++ 真题/华为od真题.py
@@ -801,3 +801,40 @@ else:
         else:
             stack.append(ch)
     print(len(stack))
+
+"""
+给定一个随机的整数数组(可能存在正整数和负整数)nums,
+请你在该数组中找出两个数，其和的绝对值(|nums[x]+nums[y]|)为最小值
+并返回这两个数(按从小到大返回)以及绝对值。
+每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+输入描述：
+ 一个通过空格空格分割的有序整数序列字符串，最多1000个整数，
+ 且整数数值范围是[-65535,65535]
+
+输出描述：
+  两个数和两数之和绝对值
+
+ 示例一：
+  输入
+  -1 -3 7 5 11 15
+  输出
+  -3 5 2
+
+说明：
+因为|nums[0]+nums[2]|=|-3+5|=2最小，
+所以返回-3 5 2
+"""
+
+nums = list(map(int, input().split()))
+nums.sort()
+n = len(nums)
+min_value, l, r = float("inf"), None, None
+
+for i in range(n):
+    for j in range(i + 1, n):
+        if abs(nums[i] + nums[j]) < min_value:
+            min_value = abs(nums[i] + nums[j])
+            l, r = nums[i], nums[j]
+if min_value != float("inf"):
+    print(f"{l} {r} {min_value}")

7a91db5e5b9e0b1dad9dc603e4f68d27ea7390de
uian23 <xtbo9719@gmail.com>
2022-08-13 14:52:57 +0800

    真题57

--- 真题/华为od真题.py
+++ 真题/华为od真题.py
@@ -339,3 +339,55 @@ else:
     sum_time += current_jobs[-1]
 
     print(sum_time)
+
+"""
+给定一个字符串
+只包含大写字母
+求在包含同一字母的子串中
+长度第K长的子串
+相同字母只取最长的子串
+
+输入
+第一行 一个子串 1<len<=100
+只包含大写字母
+第二行为k的值
+
+输出
+输出连续出现次数第k多的字母的次数
+
+例子：
+输入
+        AABAAA
+        2
+输出
+        1
+同一字母连续出现最多的A 3次
+第二多2次  但A出现连续3次
+
+输入
+
+AAAAHHHBBCDHHHH
+3
+
+输出
+2
+
+//如果子串中只包含同一字母的子串数小于k
+
+则输出-1
+"""
+s = input() + "a"
+k = int(input())
+left = right = 0
+d = {}
+while right < len(s):
+    if s[left] == s[right]:
+        right += 1
+    else:
+        d[s[left]] = max(d.get(s[left], 0), right - left)
+        left = right
+tmp = sorted(d.items(), key=lambda x: x[1], reverse=True)
+if k > len(tmp):
+    print(-1)
+else:
+    print(tmp[k - 1][1])

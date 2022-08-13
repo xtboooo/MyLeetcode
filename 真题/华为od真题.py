5c02f7e68566248db72e23f957c453b3c4f085cf
uian23 <xtbo9719@gmail.com>
2022-08-13 11:11:56 +0800

    真题69

--- 真题/华为od真题.py
+++ 真题/华为od真题.py
@@ -774,3 +774,30 @@ if n in d:
         print(m * d[n][-1])
 else:
     print(-1)
+
+"""
+游戏规则：输入一个只包含英文字母的字符串，
+字符串中的俩个字母如果相邻且相同，就可以消除。
+在字符串上反复执行消除的动作，
+直到无法继续消除为止，
+此时游戏结束。
+输出最终得到的字符串长度。
+
+输出：原始字符串str只能包含大小写英文字母，字母的大小写敏感，长度不超过100，
+输出游戏结束后字符串的长度
+
+备注：输入中包含非大小写英文字母是均为异常输入，直接返回0。
+
+事例：mMbccbc输出为3
+"""
+s = input()
+if not s.isalpha():
+    print(0)
+else:
+    stack = []
+    for ch in s:
+        if stack and ch == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(ch)
+    print(len(stack))

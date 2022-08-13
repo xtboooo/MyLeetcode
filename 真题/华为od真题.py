65f7ff0ee4eea3a6e4fe657dc992e5dd560603f4
uian23 <xtbo9719@gmail.com>
2022-08-13 13:37:27 +0800

    真题62

--- 真题/华为od真题.py
+++ 真题/华为od真题.py
@@ -94,3 +94,73 @@ d = {s.split(":")[0]: int(s.split(":")[1]) for s in all.split(",")}
 for s in used.split(","):
     d[s.split(":")[0]] -= int(s.split(":")[1])
 print(",".join(f"{k}:{v}" for k, v in d.items() if v > 0))
+
+"""
+有一种简易压缩算法：针对全部为小写英文字母组成的字符串，
+将其中连续超过两个相同字母的部分压缩为连续个数加该字母
+其他部分保持原样不变.
+例如字符串aaabbccccd  经过压缩变成字符串 3abb4cd
+请您编写解压函数,根据输入的字符串,
+判断其是否为合法压缩过的字符串
+若输入合法则输出解压缩后的字符串
+否则输出字符串"!error"来报告错误
+
+输入描述
+输入一行，为一个ASCII字符串
+长度不超过100字符
+用例保证输出的字符串长度也不会超过100字符串
+
+输出描述
+若判断输入为合法的经过压缩后的字符串
+则输出压缩前的字符串
+若输入不合法 则输出字符串"!error"
+
+示例一：
+输入
+4dff
+输出
+ddddff
+说明
+4d扩展为4个d ，故解压后的字符串为ddddff
+
+示例二
+输入
+    2dff
+输出
+    !error
+说明
+    2个d不需要压缩 故输入不合法
+
+示例三
+输入
+4d@A
+输出
+!error
+说明
+    全部由小写英文字母做成的字符串，压缩后不会出现特殊字符@和大写字母A
+    故输入不合法
+"""
+
+
+def un_zip(string: str) -> str:
+    left = right = 0
+    lst = []
+    while right < len(string):
+        if string[left].isdigit():
+            right += 1
+            if string[right].isalpha():
+                lst.append(f"{string[right]}" * int(string[left]))
+                right += 1
+                left = right
+        else:
+            lst.append(string[right])
+            right += 1
+            left = right
+    unzip_string = "".join(lst)
+    if not unzip_string.isalpha():
+        return "!error"
+    else:
+        return unzip_string
+
+
+print(un_zip(input()))
